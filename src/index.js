f745437fee1fe53217dcbf3dac620253c1120539
Marisa You <marisayou2017@u.northwestern.edu>
2020-12-01 23:52:47 -0800

    fix bug to render correct lists for each recipient

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const recipientsList = document.getElementById('recipientsList');
     const addRecipientModal = document.getElementById('addRecipientModal');
     const addRecipientForm = document.getElementById('addRecipientForm');
+    const toBuyList = document.getElementById('to-buy-list');
+    const boughtList = document.getElementById('bought-list');
     let allRecipients; 
 
-    const cartIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    const cartIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-        <path fill-rule="evenodd" d="M11.354 5.646a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708 0z"/>
     </svg>`;
 
-    const cartIcon2 = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+    const filledCartIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
     </svg>`;
 
     const removeIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -22,9 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
         <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
     </svg>`;
 
-    const removeIcon2 = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-        <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+    const filledRemoveIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
     </svg>`;
 
     // get recipients from db
@@ -74,9 +74,10 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(recipient => {
             addRecipientToList(recipient);
             allRecipients.push(recipient);
-            if (allRecipients.length === 1) {
-                renderRecipient(recipient);
-            }
+            // if (allRecipients.length === 1) {
+            //     renderRecipient(recipient);
+            // }
+            renderRecipient(recipient);
         });
         
     }
@@ -87,6 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
         recipientLi.className = 'list-group-item';
         recipientLi.innerText = recipient.name;
         recipientsList.appendChild(recipientLi);
+        //renderRecipient(recipient);
     }
 
     // click on recipient to view his/her lists
@@ -102,10 +104,13 @@ document.addEventListener('DOMContentLoaded', function() {
         const recipientName = document.getElementById('recipient-name');
         recipientName.innerText = recipient.name;
 
+        toBuyList.innerHTML = '';
+        boughtList.innerHTML = '';
+
         const recipientItems = recipient.recipient_items;
         let toBuyItems = [];
         let boughtItems = [];
-
+        console.log(recipient);
         for (const item of recipientItems) {
             if (item.bought) {
                 boughtItems.push(item);
@@ -114,16 +119,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 toBuyItems.push(item);
             }
         }
-        const toBuyList = document.getElementById('to-buy-list');
+        
         renderListItems(toBuyList, toBuyItems);
-        const boughtList = document.getElementById('bought-list');
         renderListItems(boughtList, boughtItems);
 
     }
 
     function renderListItems(ul, recipientItems) {
         const listType = ul.id;
-        ul.innerHTML = ""
         console.log(ul)
         
         for (let i = 0; i < recipientItems.length; i++) {
@@ -134,20 +137,30 @@ document.addEventListener('DOMContentLoaded', function() {
             itemRow.className = 'row';
 
             const itemNameCol = document.createElement('div');
-            itemNameCol.className = 'col'; 
+            itemNameCol.className = 'col list-item'; 
 
             const itemPriceCol = document.createElement('div');
-            itemPriceCol.className = 'col text-right';
+            itemPriceCol.className = 'col text-right list-item';
 
             const cartBtnCol = document.createElement('div');
             if (listType === 'to-buy-list') {
-                cartBtnCol.className = 'col-1';
-                cartBtnCol.innerHTML = cartIcon2;
+                cartBtnCol.className = 'col-1 icon-col';
+
+                const cartBtn = document.createElement('button');
+                cartBtn.className = 'btn icon-btn';
+                cartBtn.innerHTML = cartIcon;
+
+                cartBtnCol.appendChild(cartBtn);
             }
 
             const removeBtnCol = document.createElement('div');
-            removeBtnCol.className = 'col-1';
-            removeBtnCol.innerHTML = removeIcon;
+            removeBtnCol.className = 'col-1 icon-col';
+
+            const removeBtn = document.createElement('button');
+            removeBtn.className = 'btn icon-btn';
+            removeBtn.innerHTML = removeIcon;
+            
+            removeBtnCol.appendChild(removeBtn);
 
             fetch(itemsURL + '/' + recipientItems[i].item_id)
             .then(res => res.json())
